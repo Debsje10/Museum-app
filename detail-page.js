9101b96afb517f8f741c1d108e34b82eab186875
debsje10 <deborah.vd.rhee@gmail.com>
2019-10-11 19:50:13 +0200

    made background color-flow and thumbs ups and down

--- detail-page.js
+++ detail-page.js
@@ -8,8 +8,11 @@ function submitComment()
     let h3 = document.createElement('h3')
     let p = document.createElement('p')
 
+    var newMsg = msg.replace(/fuck/g,'love').replace(/ik/g,'jij')
+    
+
     h3.innerHTML = `${name} said:`
-    p.innerHTML = msg
+    p.innerHTML = newMsg
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
@@ -20,18 +23,23 @@ function submitComment()
    inputEvent.value = null
     textArea.value = null
 
+
     function doesNotPassAllValidaions(name, msg) {
 
-        if (!name) {
-            alert("Fill out your name")
-           
+
+        
+
+        if (name === "") {
+            alert("Fill out your name") 
+            
         }
-        if (!msg)
+        else (msg==="") 
             alert("no empty message!")
+            return true;
            
         }
         
-       if (!name || !msg) {
+        if (!name || !msg) {
        alert("No blank messages!");
        return true;
     }
@@ -41,16 +49,20 @@ function submitComment()
         alert("No messages longer than 80 characters!");
         return true;
     }
-
     return false
 }
 
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
 
 
 
 
 
 
+  let picture1 = document.getElementById('foto1')
+  onclick
 
 
     
