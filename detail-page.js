22cc4586d2010fd32f51d741effbc931933ccb41
debsje10 <deborah.vd.rhee@gmail.com>
2019-10-14 14:08:49 +0200

    deleted branche everything up to date

---
+++ detail-page.js
@@ -0,0 +1,68 @@
+function submitComment() 
+{
+    let inputEvent = document.getElementById('name')
+    let name = inputEvent.value
+    let textArea = document.getElementById('msg')
+    let msg = textArea.value
+    let comment = document.createElement('section')
+    let h3 = document.createElement('h3')
+    let p = document.createElement('p')
+
+    var newMsg = msg.replace(/fuck/g,'love').replace(/jij/g,'ik').replace(/kuthoer/g,'lieverd').replace(/kut/g,'lief')
+    
+
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = newMsg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+    
+    let commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+
+   inputEvent.value = null
+    textArea.value = null
+
+
+    function doesNotPassAllValidaions(name, msg) {
+
+
+        
+
+        if (name === "") {
+            alert("Fill out your name") 
+            
+        }
+        else (msg==="") 
+            alert("no empty message!")
+            return true;
+           
+        }
+        
+        if (!name || !msg) {
+       alert("No blank messages!");
+       return true;
+    }
+
+
+    if (msg.length > 80) {
+        alert("No messages longer than 80 characters!");
+        return true;
+    }
+    return false
+}
+
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
+
+
+
+
+
+
+  let picture1 = document.getElementById('foto1')
+  onclick
+
+
+    
